a4af044439785b740dc2e48feb54152de6270aec
siddimore <siddimore@gmail.com>
2018-08-15 21:50:27 -0700

    Add WebSocket Subscribe API To Get BTCUSD Pairing from GDAX

---
+++ gdax_websocket.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import json, time
+import websocket
+
+import time
+import json
+import thread
+
+from threading import Thread
+
+def on_message(ws, message):
+    print(message)
+
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws):
+    print("### closed ###")
+
+
+def on_open(ws):
+    print("ONOPEN")
+    def run(*args):
+        message = {
+            "type": "subscribe",
+            "channels": [{"name": "ticker", "product_ids": ["BTC-USD"]}]
+        }
+        ws.send(json.dumps(message))
+        while True:
+            time.sleep(1)
+        ws.close()
+        print("thread terminating...")
+    thread.start_new_thread(run, ())
+
+
+if __name__ == "__main__":
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("wss://ws-feed.gdax.com",
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
